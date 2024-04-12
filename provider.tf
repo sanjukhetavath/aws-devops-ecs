dc7c017a4ee0e387004ddaaca3055aca49e740f9
sanju khetavath <rathodsanju399@gmail.com>
2024-04-12 10:48:53 +1000

    update

--- provider.tf
+++ provider.tf
@@ -12,9 +12,3 @@ provider "aws" {
   region     = "ap-southeast-2"
 }
 
- backend "s3" {
-    bucket  = "asgterraformremotestate"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
